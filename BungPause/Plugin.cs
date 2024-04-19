006cd2990f14ba082d4aeb5ebab4142a43b9daa7
Raine Aeternal <64136899+raineio@users.noreply.github.com.>
2024-04-19 23:54:50 +0200

    this now uses an sdk style project, and uses sirautil for zenject.

--- BungPause/Plugin.cs
+++ BungPause/Plugin.cs
@@ -1,12 +1,7 @@
-﻿using IPA;
-using IPA.Config;
-using IPA.Config.Stores;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using System;
+using IPA;
+using SiraUtil.Logging;
+using SiraUtil.Zenject;
 using IPALogger = IPA.Logging.Logger;
 
 namespace BungPause
@@ -14,35 +9,30 @@ namespace BungPause
     [Plugin(RuntimeOptions.SingleStartInit)]
     public class Plugin
     {
-        internal static Plugin Instance { get; private set; }
-        internal static IPALogger Log { get; private set; }
-
+        public static IPALogger Logger;
+        private SiraLog _log;
+        
+        
         [Init]
         /// <summary>
         /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
         /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
         /// Only use [Init] with one Constructor.
         /// </summary>
-        public void Init(IPALogger logger)
+        public void Init(IPALogger logger, Zenjector zenjector)
         {
-            Instance = this;
-            Log = logger;
-            Log.Info("BungPause initialized.");
+            Logger = logger;
+            
+            zenjector.UseLogger(Logger);
+            
+            zenjector.Install(Location.GameCore, container =>
+            {
+                container.BindInterfacesAndSelfTo<BungPauseController>().AsSingle().NonLazy();
+            });
         }
 
-        [OnStart]
-        public void OnApplicationStart()
-        {
-            Log.Debug("OnApplicationStart");
-            new GameObject("BungPauseController").AddComponent<BungPauseController>();
-
-        }
-
-        [OnExit]
-        public void OnApplicationQuit()
-        {
-            Log.Debug("OnApplicationQuit");
-
-        }
+        [OnEnable, OnDisable]
+        public void OnStateChanged()
+        { }
     }
 }
